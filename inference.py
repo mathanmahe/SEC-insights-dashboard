2bcb3f57fe7cc20188f6e2de8a17d385f7774b97
Mathan <mathanmahe99@gmail.com>
2024-05-01 10:43:11 -0400

    add code for inference

---
+++ inference.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+def query(payload, model_id, api_token):
+    headers = {"Authorization": f"Bearer {api_token}"}
+    API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-ru-en"
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+model_id = "distilbert-base-uncased"
+api_token = "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" # get yours at hf.co/settings/tokens
+# data = query("The goal of life is [MASK].", model_id, api_token)
+
+file_path = "clean/sec-edgar-filings/AAPL/10-K/0000320193-17-000070/full-submission.txt"
+
+with open(file_path, 'r') as file:
+    text = file.read()
+
+print(len(text.split()))
+
+
+data = query({"inputs": text}, model_id, api_token)
+print(data)
