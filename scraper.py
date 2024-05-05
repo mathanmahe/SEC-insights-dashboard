c97d5b857ee031a61759d048df5b351bc9f1e4ef
Mathan <mathanmahe99@gmail.com>
2024-05-05 09:32:37 -0400

    add output dir as function argument

--- scraper.py
+++ scraper.py
@@ -5,21 +5,22 @@ import os
 from sec_edgar_downloader import Downloader
 
 class Scraper:
-    def __init__(self, company_ticker, start_date, end_date):
+    def __init__(self, company_ticker, start_date, end_date, output_dir="./data"):
         # enter the class level variables that you would like to be used here
         self.company_ticker = company_ticker
         self.start_date = start_date
         self.end_date = end_date
+        self.output_dir = output_dir
 
     def data_scraper(self):
-        output_dir = "./data"
-        # os.path.join("./data", self.company_ticker)
+        os.path.join(self.output_dir, self.company_ticker)
         
-        # # Create the output directory if it doesn't exist
-        # if not os.path.exists(output_dir):
-        #     os.makedirs(output_dir)
+        # Create the output directory if it doesn't exist
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
 
-        downloader = Downloader("Student", "mathanmahe99@gmail.com", output_dir)
-        downloader.get("10-K", self.company_ticker, after=self.start_date, before=self.end_date)
+        downloader = Downloader("Student", "mathanmahe99@gmail.com", self.output_dir)
+        response = downloader.get("10-K", self.company_ticker, after=self.start_date, before=self.end_date, download_details=True)
+        return response
 
         
